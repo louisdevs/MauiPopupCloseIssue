c04f519f6f0e8d3c3589733898445f8e857dc962
Ostap Demkiv <o.demkiv@intetics.com>
2024-07-30 23:46:19 +0300

    commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace PopupCloseIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var content = new ContentView() { BackgroundColor = Colors.Red, WidthRequest = 100, HeightRequest = 100 };
+            content.Content = new Label() { Text = "popup" };
+            var popup = new Popup() { CanBeDismissedByTappingOutsideOfPopup = true, Content = content };
+
+            this.ShowPopup(popup);
+        }
+    }
+}
